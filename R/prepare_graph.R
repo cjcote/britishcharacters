bfc9ee7fcaef477ac21235b46e856f26e47f2e83
Chris Cote <christopher_cote@hks16.harvard.edu>
2020-02-19 23:23:23 -0500

    exporting prepare_graph ...

--- R/prepare_graph.R
+++ R/prepare_graph.R
@@ -2,8 +2,10 @@ library(dplyr)
 library(ggraph)
 library(tidygraph)
 library(stringr)
-mapp_episode <- function(show_selected, season_selected, episode_selected) {
-  characters %>%
+
+
+map_episode <- function(show_selected, season_selected, episode_selected) {
+   characters %>%
     dplyr::group_by(character, show) %>%
     dplyr::count() %>%
     dplyr::group_by(character) %>%
@@ -79,111 +81,111 @@ mapp_episode <- function(show_selected, season_selected, episode_selected) {
     geom_node_label(aes(label = character_label), size = 3)+
     theme_void()
 }
-mapp_episode("Vera", 2, 1)
 
-characters %>%
-  dplyr::group_by(character, show) %>%
-  dplyr::count() %>%
-  dplyr::group_by(character) %>%
-  dplyr::mutate(total_shows = n()) %>%
-  dplyr::arrange(-total_shows) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(total_shows > 1) %>%
-  filter(character %in% (
-    characters %>%
-      dplyr::filter(show == "Vera",
-                    season == 2,
-                    episode == 1) %>%
-      dplyr::distinct(character) %>% pull(character)
-  ) & show == "Vera")
-
-
-characters %>%
-  dplyr::group_by(character, show) %>%
-  dplyr::count() %>%
-  dplyr::group_by(character) %>%
-  dplyr::mutate(total_shows = n()) %>%
-  dplyr::arrange(-total_shows) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(total_shows > 1) %>%
-  dplyr::rename(to = character, from = show) %>%
-  dplyr::arrange(from)  %>%
-  left_join(
-    characters %>%
-      dplyr::group_by(character, show) %>%
-      dplyr::count() %>%
-      dplyr::group_by(character) %>%
-      dplyr::mutate(total_shows = n()) %>%
-      dplyr::arrange(-total_shows) %>%
-      dplyr::ungroup() %>%
-      dplyr::filter(total_shows > 1) %>%
-      dplyr::rename(to = character, from = show) %>%
-      dplyr::arrange(from)  %>%
-      distinct(to) %>%
-      mutate(to_index = 1:n())
-  ) %>%
-  left_join(
-    characters %>%
-      dplyr::group_by(character, show) %>%
-      dplyr::count() %>%
-      dplyr::group_by(character) %>%
-      dplyr::mutate(total_shows = n()) %>%
-      dplyr::arrange(-total_shows) %>%
-      dplyr::ungroup() %>%
-      dplyr::filter(total_shows > 1) %>%
-      dplyr::rename(to = character, from = show) %>%
-      dplyr::arrange(from)  %>%
-      distinct(from) %>%
-      mutate(from_index = 1:n())
-  ) -> prepared_tbl
+# characters %>%
+#   dplyr::group_by(character, show) %>%
+#   dplyr::count() %>%
+#   dplyr::group_by(character) %>%
+#   dplyr::mutate(total_shows = n()) %>%
+#   dplyr::arrange(-total_shows) %>%
+#   dplyr::ungroup() %>%
+#   dplyr::filter(total_shows > 1) %>%
+#   filter(character %in% (
+#     characters %>%
+#       dplyr::filter(show == "Vera",
+#                     season == 2,
+#                     episode == 1) %>%
+#       dplyr::distinct(character) %>% pull(character)
+#   ) & show == "Vera")
 
 
-prepared_tbl %>%
-  tidygraph::as_tbl_graph() %>%
-  tidygraph::activate(nodes) %>%
-  dplyr::mutate(show_label = ifelse(stringr::str_detect(name, characters %>% dplyr::distinct(show) %>%
-                                                          dplyr::pull(show) %>%
-                                                          paste(collapse = "|")), name, NA),
-                character_label = ifelse(name %in% (
-                  characters %>%
-                    dplyr::filter(show == "Vera",
-                                  season == 2,
-                                  episode == 1) %>%
-                    dplyr::distinct(character) %>% pull(character)
-                ), name, NA)) %>%
-  activate(edges) %>%
-  left_join(prepared_tbl %>%
-              distinct(from, from_index) %>%
-              rename(show = from,
-                   from  = from_index)) %>%
-  left_join(prepared_tbl %>%
-              distinct(to, to_index) %>%
-              rename(character = to,
-                     to = to_index)) %>%
-  mutate(selected = ifelse(character %in% (
-    characters %>%
-      dplyr::filter(show == "Vera",
-                    season == 2,
-                    episode == 1) %>%
-      dplyr::distinct(character) %>% pull(character)
-  ), 1, 1/50)) %>%
-  ggraph(layout = "kk") +
-  # geom_edge_diagonal(alpha = 1/50,
-  #                    show.legend = FALSE) +
-  geom_edge_diagonal(aes(color = factor(selected), alpha = selected)) +
-  geom_node_label(aes(label = show_label), fill = "black", color = "white", alpha = 3/5) +
-  geom_node_label(aes(label = character_label), size = 3)+
-  theme_void()
-
+# characters %>%
+#   dplyr::group_by(character, show) %>%
+#   dplyr::count() %>%
+#   dplyr::group_by(character) %>%
+#   dplyr::mutate(total_shows = n()) %>%
+#   dplyr::arrange(-total_shows) %>%
+#   dplyr::ungroup() %>%
+#   dplyr::filter(total_shows > 1) %>%
+#   dplyr::rename(to = character, from = show) %>%
+#   dplyr::arrange(from)  %>%
+#   left_join(
+#     characters %>%
+#       dplyr::group_by(character, show) %>%
+#       dplyr::count() %>%
+#       dplyr::group_by(character) %>%
+#       dplyr::mutate(total_shows = n()) %>%
+#       dplyr::arrange(-total_shows) %>%
+#       dplyr::ungroup() %>%
+#       dplyr::filter(total_shows > 1) %>%
+#       dplyr::rename(to = character, from = show) %>%
+#       dplyr::arrange(from)  %>%
+#       distinct(to) %>%
+#       mutate(to_index = 1:n())
+#   ) %>%
+#   left_join(
+#     characters %>%
+#       dplyr::group_by(character, show) %>%
+#       dplyr::count() %>%
+#       dplyr::group_by(character) %>%
+#       dplyr::mutate(total_shows = n()) %>%
+#       dplyr::arrange(-total_shows) %>%
+#       dplyr::ungroup() %>%
+#       dplyr::filter(total_shows > 1) %>%
+#       dplyr::rename(to = character, from = show) %>%
+#       dplyr::arrange(from)  %>%
+#       distinct(from) %>%
+#       mutate(from_index = 1:n())
+#   ) -> prepared_tbl
+#
+#
+# prepared_tbl %>%
+#   tidygraph::as_tbl_graph() %>%
+#   tidygraph::activate(nodes) %>%
+#   dplyr::mutate(show_label = ifelse(stringr::str_detect(name, characters %>% dplyr::distinct(show) %>%
+#                                                           dplyr::pull(show) %>%
+#                                                           paste(collapse = "|")), name, NA),
+#                 character_label = ifelse(name %in% (
+#                   characters %>%
+#                     dplyr::filter(show == "Vera",
+#                                   season == 2,
+#                                   episode == 1) %>%
+#                     dplyr::distinct(character) %>% pull(character)
+#                 ), name, NA)) %>%
+#   activate(edges) %>%
+#   left_join(prepared_tbl %>%
+#               distinct(from, from_index) %>%
+#               rename(show = from,
+#                    from  = from_index)) %>%
+#   left_join(prepared_tbl %>%
+#               distinct(to, to_index) %>%
+#               rename(character = to,
+#                      to = to_index)) %>%
+#   mutate(selected = ifelse(character %in% (
+#     characters %>%
+#       dplyr::filter(show == "Vera",
+#                     season == 2,
+#                     episode == 1) %>%
+#       dplyr::distinct(character) %>% pull(character)
+#   ), 1, 1/50)) %>%
+#   ggraph(layout = "kk") +
+#   # geom_edge_diagonal(alpha = 1/50,
+#   #                    show.legend = FALSE) +
+#   geom_edge_diagonal(aes(color = factor(selected), alpha = selected)) +
+#   geom_node_label(aes(label = show_label), fill = "black", color = "white", alpha = 3/5) +
+#   geom_node_label(aes(label = character_label), size = 3)+
+#   theme_void()
 
 
 
+#' @export
 find_characters <- function(show_selected, season_selected, episode_selected) {
   characters %>% filter(show == show_selected, season == season_selected,
                         episode == episode_selected) %>%
     distinct(character) %>% pull(character)
 }
 
+#' @export
 find_distinct_shows <- function(character_list){
   characters %>%
     filter(character %in% character_list) %>%
@@ -192,18 +194,18 @@ find_distinct_shows <- function(character_list){
     arrange(-n_show)
 }
 
-find_distinct_shows(find_characters("Bletchley Circle", 1, 3)) %>%
-  select(show, character) %>%
-  distinct(character) %>%
-  pull(character) %>%
-  paste(collapse = "|") %>%
-  prepare_graph() %>%
-  graph_prepared_graph()
+# find_distinct_shows(find_characters("Bletchley Circle", 1, 3)) %>%
+#   select(show, character) %>%
+#   distinct(character) %>%
+#   pull(character) %>%
+#   paste(collapse = "|") %>%
+#   prepare_graph() %>%
+#   graph_prepared_graph()
 
 
-
+#' @export
 prepare_graph <- function(selected_name){
-prepared_tbl %>%
+  prepared_tbl %>%
   select(from, to) %>%
   as_tbl_graph() %>%
   activate(nodes) %>%
@@ -220,7 +222,7 @@ prepared_tbl %>%
   arrange(-selected_edge)
 }
 
-
+#' @export
 graph_prepared_graph <- function(prepared_tbl_graph){
   prepared_tbl_graph %>%
   ggraph(layout = "kk") +
